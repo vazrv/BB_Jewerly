42d5c35f58cb79b8cd89bac9bb63d0b2a2dbefee
toshk0 <toshk0@vk.com>
2024-11-23 17:26:32 +0300

    formated

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,7 @@
 export const Button = ({ children }) => {
-    return (
-        <button className="border border-white rounded-md w-48 h-12 flex items-center justify-center py-2 pl-4 text-white">
-            {children}
-        </button>
-    );
+  return (
+    <button className="border border-white rounded-md w-48 h-12 flex items-center justify-center py-2 pl-4 text-white">
+      {children}
+    </button>
+  );
 };
