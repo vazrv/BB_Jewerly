df089f9ca9ea63a4b28722396e6a67ea315fce12
toshk0 <toshk0@vk.com>
2024-11-23 17:30:14 +0300

    rewrite with router link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,8 @@
+// libs
 import React from "react";
+import { Link } from "react-router-dom";
+
+// Icons
 import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa";
 import like from "@assets/icons/like.svg";
 import cart from "@assets/icons/cart.svg";
@@ -10,40 +14,40 @@ const Header = () => {
     <header>
       <div className="bg-mainColor font-cormorantSC">
         <div className="container mx-auto py-4">
-          <div className="flex justify-between items-center">
+          <div className="flex items-center justify-between">
             <nav>
               <ul className="flex gap-9 text-[1.25rem]">
                 <li>
-                  <a href="#">О нас</a>
+                  <Link to="#">О нас</Link>
                 </li>
                 <li>
-                  <a href="#">Каталог</a>
+                  <Link to="#">Каталог</Link>
                 </li>
                 <li>
-                  <a href="#">Блог</a>
+                  <Link to="#">Блог</Link>
                 </li>
               </ul>
             </nav>
-            <h1 className="text-[1.6875rem] font-regular pl-36">BB Jewerly</h1>
-            <div className="flex gap-5 items-center">
-              <div className="flex border-[0.0625rem] border-black text-black rounded-lg py-[0.3125rem] bg-transparent  items-center justify-center w-fit ">
+            <h1 className="font-regular pl-36 text-[1.6875rem]">BB Jewerly</h1>
+            <div className="flex items-center gap-5">
+              <div className="flex w-fit items-center justify-center rounded-lg border-[0.0625rem] border-black bg-transparent py-[0.3125rem] text-black">
                 <input
                   type="text"
                   placeholder="Поиск"
-                  className="bg-transparent placeholder:font-sans outline-none px-2 w-[10rem] font-karla placeholder-[#727272]"
+                  className="w-[10rem] bg-transparent px-2 font-karla placeholder-[#727272] outline-none placeholder:font-sans"
                 />
                 <img src={search} alt="" className="mr-2" />
               </div>
-              <div className="flex gap-9 items-center">
-                <a href="">
+              <div className="flex items-center gap-9">
+                <Link to="">
                   <img src={like} alt="" className="ml-4" />
-                </a>
-                <a href="">
+                </Link>
+                <Link to="">
                   <img src={cart} alt="" />
-                </a>
-                <a href="">
+                </Link>
+                <Link to="">
                   <img src={profile} alt="" className="pb-1" />
-                </a>
+                </Link>
               </div>
             </div>
           </div>
